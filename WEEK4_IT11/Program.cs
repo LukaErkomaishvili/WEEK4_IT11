781308a70225b849fbc6fd73b2f3e1d33786dd62
LukaErkomaishvili <luka.erkomaishvili02@gmail.com>
2025-09-15 12:01:03 +0400

    First VS Push

---
+++ WEEK4_IT11/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace WEEK4_IT11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
